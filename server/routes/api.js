de884489a3cfd84d7d7dd8ec20f288e67e586d17
DHAPARK <pdh4182@gmail.com>
2022-11-17 21:20:29 +0900

    what?

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const session = require("express-session");
 const Study = require("../models/study");
 const StudyAttendsStatus = require("../models/studyAttendsStatus");
 const { route } = require("./page");
