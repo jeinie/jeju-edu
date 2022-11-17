fd25c1beca9c15058bfc5bcd7e5d171dee900312
DHAPARK <pdh4182@gmail.com>
2022-11-18 06:55:59 +0900

    백단단

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const Study = require("../models/study");
 const StudyAttendsStatus = require("../models/studyAttendsStatus");
-const 
+
 const { Op } = require("sequelize");
 const { route } = require("./page");
 
