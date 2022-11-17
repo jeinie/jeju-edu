dd723bdd77d604603705774897c9a5ebe2e2c197
DHAPARK <pdh4182@gmail.com>
2022-11-17 15:44:38 +0900

    protocol처리

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,36 +6,50 @@ const User = require("../models/user");
 
 const router = express.Router();
 
-router.post("/join", async (req, res, next) => {
+router.post("/api/join", async (req, res, next) => {
+  const result = {};
   const { id, password, name } = req.body;
   try {
     const exUser = await User.findOne({ where: { id } });
     if (exUser) {
-      res.send("유저 중복");
+      result["success"] = 100;
+      result["msg"] = "아이디 중복입니다";
+      res.json(result);
+      return;
     }
-
     await User.create({
       id,
       password,
       name,
     });
     console.log(`회원가입 정보\nid : ${id}\npw : ${password}\nname : ${name}`);
+    result["success"] = 200;
+    result["msg"] = "회원가입 성공";
+    res.json(result);
   } catch (error) {
     console.error(error);
     return next(error);
   }
 });
 
-router.post("/login", async (req, res, next) => {
+router.post("/api/login", async (req, res, next) => {
   const { id, password } = req.body;
+  const result = {};
   try {
     const exUser = await User.findOne({
       where: { id: id, password: password },
     });
-
-    req.session.userInfo = exUser.dataValues;
-
-    console.log(`로그인 확인 ${JSON.stringify(req.session.userInfo)}`);
+    if (exUser) {
+      req.session.userInfo = exUser.dataValues;
+      result["success"] = 200;
+      result["msg"] = "로그인 성공";
+      console.log(`로그인 확인 ${JSON.stringify(req.session.userInfo)}`);
+      res.json(result);
+    } else {
+      result["success"] = 100;
+      result["msg"] = "로그인 실패";
+      res.json(result);
+    }
   } catch (error) {
     console.error(error);
     return next(error);
@@ -43,10 +57,12 @@ router.post("/login", async (req, res, next) => {
 });
 
 router.get("/logout", (req, res) => {
-  //req.logout();
+  const result = {};
+  result["success"] = 200;
+  result["msg"] = "로그아웃 완료";
   req.session.destroy();
-  //res.redirect("/");
   console.log(`로그아웃 확인${req}`);
+  res.json(result);
 });
 
 module.exports = router;
