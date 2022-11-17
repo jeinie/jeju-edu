768f8fe620102a2fd0430b340d0e50f54ebb6b28
DHAPARK <pdh4182@gmail.com>
2022-11-17 19:50:29 +0900

    참석자테이블추가

--- server/models/studyAttendsStatus.js
+++ server/models/studyAttendsStatus.js
@@ -6,6 +6,7 @@ module.exports = class StudyAttendsStatus extends Sequelize.Model {
       {
         idx: {
           type: Sequelize.INTEGER,
+          autoIncrement: true,
           primaryKey: true,
         },
         study_no: {
