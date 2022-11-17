34a059d05b1a56e780b08935ecb60afbd7e241e3
DHAPARK <pdh4182@gmail.com>
2022-11-17 23:02:32 +0900

    처리

--- server/routes/api.js
+++ server/routes/api.js
@@ -75,8 +75,8 @@ router.post("/joinStudy", async (req, res, next) => {
   console.log(req.body);
   try {
     await StudyAttendsStatus.create({
-      study_no: req.body.study_no,
-      id: req.body.id,
+      study_no: req.body.body.study_no,
+      id: req.body.body.id,
     });
     result["success"] = 200;
     result["msg"] = "study 테이블 join 성공";
