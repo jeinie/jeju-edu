469433490a629d0d43f24c1467aa3aba8e9beecb
hyunjun Yang <yhj0439@nate.com>
2022-11-17 19:01:06 +0900

    bro pull

--- client/src/page/detail/PartyDetail.js
+++ client/src/page/detail/PartyDetail.js
@@ -10,13 +10,16 @@ import Input from "./../../components/Input";
 export default function PartyDetail({ list }) {
   const { id } = useParams();
   const [personalList, setPersonalList] = useState(null);
-  // useEffect(() => {
-  //   axios
-  //     .post(`http://13.125.223.194:56742/viewDetail/${id}`)
-  //     .then((data) => setPersonalList(data));
-  // }, []);
-  console.log(id);
-  // console.log(personalList);
+  useEffect(() => {
+    axios
+      .post(`http://13.125.223.194:56742/viewDetail/${id}`)
+      .then((data) => setPersonalList(data));
+  }, []);
+  console.log(personalList);
+  if (personalList === null) {
+    return <div>리스트가 없습니다 !</div>;
+  }
+
   return (
     <div>
       <PartyMarker lat="33.450317" lon="126.570764" />
