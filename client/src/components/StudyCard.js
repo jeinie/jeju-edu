0d8d3c2698ff7f435b06c49a0b284573c3984918
chucoding <chucoding@gmail.com>
2022-11-17 12:53:36 +0900

    study card design

--- client/src/components/StudyCard.js
+++ client/src/components/StudyCard.js
@@ -1,12 +1,17 @@
 import styled from "styled-components";
+import { HeartFillIcon, LocationIcon } from "@goorm-dev/gds-goormthon";
 
 export default function StudyCard() {
 
     return (
       <StudyBox>
-        <StudyIcon/>
-        <UserName className="font"/>
-        <StudyTitle className="font-bold"/>
+        <IconBox style={{borderRadius:'50%',  background : '#D3D3D3', float:'left'}}/>
+        <IconBox style={{float:'right'}}>
+            <HeartFillIcon style={{color:"lightGray"}}/>
+        </IconBox>
+        <UserName className="font">username</UserName>
+        <StudyTitle className="font-bold">스터디</StudyTitle>
+        <LocationIcon/>
       </StudyBox>
     );
 }
@@ -17,17 +22,23 @@ const StudyBox = styled.div`
     border: 1px solid black;
 `;
 
-const StudyIcon = styled.div`
+
+const IconBox = styled.div`
     width: 24px;
     height: 24px;
-    background : #D3D3D3;
-    border-radius : 50%;
+    margin : 10px 16px;
 `;
 
-const UserName = styled.input`
+const UserName = styled.p`
     color: #000000;
+    margin-top:10px;
+    text-align:left;
+    font-size:4px;
 `;
 
-const StudyTitle = styled.input`
+const StudyTitle = styled.h1`
+    text-align:left;
     color: #000000;
+    margin-top: 4px;
+    font-size: 12px;
 `;
