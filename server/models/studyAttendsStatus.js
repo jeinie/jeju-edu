8209fec1de87343d2d9ef635124e6b266d75a859
DHAPARK <pdh4182@gmail.com>
2022-11-17 19:46:38 +0900

    db에 참석테이블 추가

---
+++ server/models/studyAttendsStatus.js
@@ -0,0 +1,32 @@
+const Sequelize = require("sequelize");
+
+module.exports = class StudyAttendsStatus extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        idx: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+        },
+        study_no: {
+          type: Sequelize.INTEGER,
+        },
+        id: {
+          type: Sequelize.STRING(20),
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "StudyAttendsStatus",
+        tableName: "studyattendsstatus",
+        paranoid: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {}
+};
