9eceadb5946c0b24cce8a12851de843a5509e2fb
chucoding <chucoding@gmail.com>
2022-11-17 10:43:21 +0900

    Study Card

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
-import Address from "./components/Address";
-import PartyMarker from "./components/PartyMarker";
 import Router from "./router";
 import "./app.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <Address />
-      <PartyMarker /> */}
       <Router />
     </div>
   );
--- client/src/page/Main/Main.js
+++ client/src/page/Main/Main.js
@@ -1,14 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
-import Footer from "../../components/Footer";
-import Input from "../../components/Input";
-import PlusBtn from "../../components/PlusBtn";
+import Footer from "../../components/Footer.js";
+import Input from "../../components/Input.js";
+import PlusBtn from "../../components/PlusBtn.js";
+import StudyCard from "../../components/StudyCard.js";
+
+function test() {
+  let array = [];
+  for (let i=0;i<3; i++) {
+    array.push(<StudyCard/>);
+  }
+  return array;
+}
 
 export default function Main() {
   return (
     <MainContainer>
       <Input />
+      <>
+      {test()}
+      </>
       <PlusBtn />
       <Footer />
     </MainContainer>
