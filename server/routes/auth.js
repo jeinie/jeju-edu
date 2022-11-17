0208e49a782fbbe706b79b06f06346bf97410113
DHAPARK <pdh4182@gmail.com>
2022-11-17 16:15:49 +0900

    변화

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ const express = require("express");
 //const bcrypt = require("bcrypt");
 //const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 const User = require("../models/user");
-
+const Study = require("../models/study");
 const router = express.Router();
 
 router.post("/api/join", async (req, res, next) => {
@@ -63,6 +63,44 @@ router.get("/logout", (req, res) => {
   req.session.destroy();
   console.log(`로그아웃 확인${req}`);
   res.json(result);
+});
+
+router.get("/api/testDBInsertStudy", async (req, res, next) => {
+  const result = {};
+  //const { id, password, name } = req.body;
+  try {
+    // const exUser = await User.findOne({ where: { id } });
+    // if (exUser) {
+    //   result["success"] = 100;
+    //   result["msg"] = "아이디 중복입니다";
+    //   res.json(result);
+    //   return;
+    // }
+    await Study.create({
+      study_name: "스터딩이름2",
+      who_open: "aaaa",
+      study_category: "gitar",
+      study_detail:
+        "studygital. \nfawfwafawf안녕 나는 지금\n.   띄어 \n 정말로? \n      콜론;   	\n정말이야.",
+      members: 7,
+      min_party: "5",
+      open_date: "2022-11-28",
+      close_date: "2022-11-30",
+      study_date: "2022-11-29",
+      location: "제주시",
+      tmX: 35,
+      tmY: 31,
+      deadline: "2022-11-31",
+      status: 0,
+    });
+
+    result["success"] = 200;
+    result["msg"] = "study 테이블 insert 성공";
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 });
 
 module.exports = router;
