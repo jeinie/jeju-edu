28b125997f9374045e693a25c1052ae6a2b52f64
DHAPARK <pdh4182@gmail.com>
2022-11-17 13:59:49 +0900

    getStudyList

---
+++ server/routes/api.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const Study = require("../models/study");
+
+const router = express.Router();
+
+router.get("/getStudyList", async (req, res, next) => {
+  try {
+    const studyList = await Study.findAll({});
+    if (studyList) {
+      console.log(`studyList는 ${studyList}`);
+      console.log(studyList);
+      res.send(JSON.stringify(studyList));
+    }
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+module.exports = router;
