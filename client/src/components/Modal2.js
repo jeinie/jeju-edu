05c54c572de71a88b363908d87b1083bd52be0a2
chucoding <chucoding@gmail.com>
2022-11-18 11:30:02 +0900

    orange modal

--- client/src/components/Modal2.js
+++ client/src/components/Modal2.js
@@ -3,18 +3,9 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+import freshMandarin_1x from '../img/freshMandarin_1x.png';
+import styled from "styled-components";
+import { borderRadius } from '@mui/system';
 
 export default function Modal2(props) {
   const handleClose = () => props.setOpen(false);
@@ -22,21 +13,41 @@ export default function Modal2(props) {
   return (
     <div>
       <Modal
-        style={{height:"100%"}}
         open={props.open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
+        <>
+        <button onClick={() => props.handleClose(false)}>click</button>
+        <img src={freshMandarin_1x} style={{display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "50vh",
+            marginTop:"150px",
+            marginLeft:"75px"}}/>
+        <button style={{
+            height:"42px",
+            marginLeft:"27px",
+            marginRight:"27px",
+            width:"84%",
+            borderRadius:"15px",
+            background:"#E47B00",
+            color:"white"
+        }} onClick={() => props.handleClose(false)}>열매를 수확해주세요.</button>
+        </>
       </Modal>
     </div>
   );
 }
+
+const CloseBtn = styled.button`
+  width: 220px;
+  height: 33px;
+  margin-top: 12px;
+  border-radius: 25px;
+  border: 1px solid #e47b00;
+  background-color: #fff;
+  color: #e47b00;
+`;
