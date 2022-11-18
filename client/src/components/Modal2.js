401ddb5c7072cc508ca5bf259894ba862ef3bc4c
chucoding <chucoding@gmail.com>
2022-11-18 11:42:25 +0900

    setting image

--- client/src/components/Modal2.js
+++ client/src/components/Modal2.js
@@ -8,6 +8,7 @@ import styled from "styled-components";
 import { borderRadius } from '@mui/system';
 
 export default function Modal2(props) {
+
   const handleClose = () => props.setOpen(false);
 
   return (
@@ -19,35 +20,36 @@ export default function Modal2(props) {
         aria-describedby="modal-modal-description"
       >
         <>
-        <button onClick={() => props.handleClose(false)}>click</button>
+        <button style={{
+            alignItems: "center",
+            height:"42px",
+            marginLeft:"145px",
+            marginRight:"27px",
+            borderRadius:"30px",
+            background:"#E47B00",
+            width:"100px",
+            color:"white",
+            marginTop:"25%"
+        }}>Click</button>
         <img src={freshMandarin_1x} style={{display: "flex",
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
             height: "50vh",
-            marginTop:"150px",
-            marginLeft:"75px"}}/>
+            marginTop:"45px",
+            marginLeft:"75px"}} />
         <button style={{
             height:"42px",
             marginLeft:"27px",
             marginRight:"27px",
+            marginTop:"30px",
             width:"84%",
             borderRadius:"15px",
             background:"#E47B00",
             color:"white"
-        }} onClick={() => props.handleClose(false)}>열매를 수확해주세요.</button>
+        }} >열매를 수확해주세요!</button>
         </>
       </Modal>
     </div>
   );
 }
-
-const CloseBtn = styled.button`
-  width: 220px;
-  height: 33px;
-  margin-top: 12px;
-  border-radius: 25px;
-  border: 1px solid #e47b00;
-  background-color: #fff;
-  color: #e47b00;
-`;
