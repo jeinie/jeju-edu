208e11162661123d6632c142b07f34b9787b5ba2
DHAPARK <pdh4182@gmail.com>
2022-11-28 04:12:15 +0900

    db정립 , jwt 완료 , 로그인/회원가입 완성

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,14 +4,18 @@ const User = require("../models/user");
 const Study = require("../models/study");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const auth = require("./authMiddleWare");
+const authMiddleWare = require("./authMiddleWare");
 const userAgentMiddleWare = require("./userAgentMiddleWare");
 
 router.post("/api/join", async (req, res, next) => {
   const result = {};
   const { id, password, name } = req.body;
+  /**
+   * 회원가입 시 비밀번호 암호화
+   */
+  const hashPw = await bcrypt.hash(password, 12);
   try {
-    const exUser = await User.findOne({ where: { id } });
+    const exUser = await User.findOne({ where: { id: id } });
     if (exUser) {
       result["success"] = 100;
       result["msg"] = "아이디 중복입니다";
@@ -20,10 +24,10 @@ router.post("/api/join", async (req, res, next) => {
     }
     await User.create({
       id,
-      password,
+      password: hashPw,
       name,
     });
-    console.log(`회원가입 정보\nid : ${id}\npw : ${password}\nname : ${name}`);
+    console.log(`회원가입 정보\nid : ${id}\npw : ${hashPw}\nname : ${name}`);
     result["success"] = 200;
     result["msg"] = "회원가입 성공";
     res.json(result);
@@ -37,47 +41,121 @@ router.post(
   "/api/login",
   userAgentMiddleWare("/api/login"),
   async (req, res, next) => {
-    const { id, password } = req.body;
-    let token = "";
+    const { id, pw } = req.body;
+    /**
+     * 로그인 시 비밀번호 암호화
+     */
+    //const hashPw = await bcrypt.hash(password, 12);
+    //const hashPw = await bcrypt.compare(password);
+
+    let accessToken = "";
+    let refreshToken = "";
+
     try {
-      const exUser = await User.findOne({
-        where: { id: id, password: password },
+      let exUser = await User.findOne({
+        where: { id: id },
       });
+
+      if (exUser == null) {
+        res.status(403).json({
+          code: 403,
+          message: "Failed to Search User",
+        });
+        return;
+      }
+
+      const Flag = await bcrypt.compare(pw, exUser.dataValues.password);
+
+      if (!Flag) {
+        res.status(403).json({
+          code: 403,
+          message: "Failed to Search User",
+        });
+        return;
+      }
+
+      /**
+       * 원래자리
+       */
+      const { password, ...others } = exUser.dataValues;
       if (exUser != null) {
-        token = jwt.sign(
+        accessToken = jwt.sign(
           {
             type: "JWT",
             id: id,
-            profile: exUser.dataValues,
+            profile: others,
           },
-          process.env.SECRET_KEY,
+          process.env.ACCESS_SECRET_KEY,
           {
-            expiresIn: "15m",
+            expiresIn: "1m",
             issuer: "admin",
           }
         );
+
+        refreshToken = jwt.sign(
+          {
+            type: "JWT",
+            id: id,
+            profile: others,
+          },
+          process.env.REFRESH_SECRET_KEY,
+          {
+            expiresIn: "2m",
+            issuer: "admin",
+          }
+        );
+
+        /**
+         * 만들어진 access, refresh 토큰 전송
+         */
+        res.cookie("accessToken", accessToken, {
+          /**
+           * https 와 http의 차이 명시
+           * http 사용임으로 false
+           */
+          secure: false,
+          /**
+           * javaScript와 http중 어디서 접근이 가능할지 명시
+           * javaScript에서 cookie접근이 불가능하게 true
+           */
+          httpOnly: true,
+        });
+
+        res.cookie("refreshToken", refreshToken, {
+          secure: false,
+          httpOnly: true,
+        });
+
         res.status(200).json({
           code: 200,
           message: "JWT Token is Created",
-          token: token,
+          accessToken: accessToken,
+          refreshToken: refreshToken,
         });
       } else {
-        res.status(200).json({
-          code: 100,
+        res.status(403).json({
+          code: 403,
           message: "Failed to Search User",
         });
       }
     } catch (error) {
       console.log(`/auth/api/login에서 에러발생 ${error}`);
-      res.status(200).json({
-        code: 101,
+      res.status(500).json({
+        code: 500,
         message: "Failed to create jwt token",
       });
     }
   }
 );
 
-router.get("/api/payload", auth, (req, res) => {
+/**
+ * url : /api/payload
+ * 토큰의 유효값을 체크하거나
+ * accessToken의 유효시간이 다되었을때 && refreshToken의 유효시간이 덜 되었을때 accessToken을 재발급하는 로직
+ * accessToken의 유효시간이 다되었을때 && refreshToken의 유효시간이 다되었을때 client에 특정값을 return하여
+ * login페이지로 redirect를 유도하는 로직이 담겨있다
+ */
+router.get("/api/payload", authMiddleWare, (req, res) => {
   const id = req.decoded.id;
   const profile = req.decoded.profile;
   return res.status(200).json({
