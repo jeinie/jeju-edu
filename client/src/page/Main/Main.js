198f95f5b5b8ac0f2bf7749ec2e16db75bf5b80e
chucoding <chucoding@gmail.com>
2022-11-30 00:38:20 +0900

    refactoring category

--- client/src/page/Main/Main.js
+++ client/src/page/Main/Main.js
@@ -1,6 +1,7 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import StudyCard from "../../components/StudyCard";
 import MainCategory from "../../components/Nav";
@@ -8,17 +9,22 @@ import Footer from "./../../components/Footer";
 import AddressInput from "../../components/maps/AddressInput";
 import MainHeader from "../../components/MainHeader";
 
-const categoryList = ["code", "sing", "design"];
-
 export default function Main() {
 
+  const [category, setCategory] = useState("code");
   const [list, setList] = useState([]);
+
+  useEffect(() => {
+     axios.get(`/api/getStudyList/${category}`).then((data) => {
+        setList(data.data);
+     });
+  }, [category]);
 
   return (
     <MainContainer>
       <MainHeader/>
       <AddressInput update={setList}/>
-      <MainCategory/>
+      <MainCategory selected={category} changeCategory={setCategory}/>
       {list.map((el, idx) => {
         return (
           <Link to={`/partydetail/${el.study_no}`} key={idx}>
