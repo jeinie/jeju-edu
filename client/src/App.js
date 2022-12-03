167e86f609f4bd974c0e6a30a39fa479a550b5eb
hyunjun Yang <yhj0439@nate.com>
2022-12-03 15:48:38 +0900

    goorm 제거

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import "./app.css";
 import "./reset.css";
 import "./font.css";
 import TransformAddress from "./components/TransformAddress";
+import AddressInput from "./components/maps/AddressInput";
+import StudyCard from "./components/StudyCard";
 
 function App() {
   return (
     <div className="App" style={{ fontFamily: "NanumSquare" }}>
       <Router />
       {/* <TransformAddress lat="33.252984" lon="126.561100" />; */}
+      {/* <AddressInput /> */}
+      {/* <StudyCard /> */}
     </div>
   );
 }
