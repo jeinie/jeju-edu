a93293e06d876cb73d12346960012b8c37a821d5
chucoding <chucoding@gmail.com>
2022-12-08 00:08:52 +0900

    refactoring

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,5 @@
-import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 
 import user from './store/userSlice.js';
