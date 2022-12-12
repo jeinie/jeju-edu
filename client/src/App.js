0656d149f9ee9e4cfa9162041eb1b0db7d5d8a32
hyunjun Yang <yhj0439@nate.com>
2022-12-12 22:13:42 +0900

    master Pull 완료

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import "./font.css";
 function App() {
   const Main = React.lazy(() => import("./page/Main/Main"));
   const Login = React.lazy(() => import("./page/login/Login"));
+  // const Join = React.lazy(() => import("./page/Join"));
   const Join = React.lazy(() => import("./page/Join"));
   const Profile = React.lazy(() => import("./page/profile/Profile"));
   const PartyJoin = React.lazy(() => import("./page/detail/PartyJoin"));
@@ -30,7 +31,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />}/>
+          <Route path="/join" element={<Join />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/partyjoin" element={<PartyJoin />} />
           <Route path={`/partydetail/:id`} element={<PartyDetail />} />
