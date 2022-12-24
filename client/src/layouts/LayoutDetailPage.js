12e176e5285f8998d1bb7329e11409b3d2b06403
ohcho-dev <ohcho.dev@gmail.com>
2022-12-24 20:42:21 +0900

    detail page z-index 수정

--- client/src/layouts/LayoutDetailPage.js
+++ client/src/layouts/LayoutDetailPage.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import styled from "styled-components";
-import Titlebar, { DetailTitlebar } from "../components/Titlebar";
-import BaseLayout from "./BaseLayout";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+import Titlebar, { DetailTitlebar } from '../components/Titlebar';
+import BaseLayout from './BaseLayout';
 
 const Content = styled.main`
   width: 100%;
-  height: ${(props) => (props.top === 0 ? "100vh" : "calc(100vh - 60px)")};
+  height: ${(props) => (props.top === 0 ? '100vh' : 'calc(100vh - 60px)')};
   position: fixed;
   top: ${(props) => props.top};
   left: 0;
-  z-index: 50;
+  z-index: 300;
   overflow-y: scroll;
   overflow-x: hidden;
   background: #fff;
 `;
-const LayoutDetailPage = ({ children, top = "60px" }) => {
+const LayoutDetailPage = ({ children, top = '60px' }) => {
   return (
     <BaseLayout>
       <DetailTitlebar />
