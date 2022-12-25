06bbe35682a7c38c9061dab296edf1ab3f8772b0
DHAPARK <pdh4182@gmail.com>
2022-12-25 14:02:50 +0900

    로그아웃이 public에선 안되는디..

--- client/src/components/func.js
+++ client/src/components/func.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { persistor } from "..";
 
 const logout = (navigate) => {
-  axios.get("/api/auth/logout").then(() => {
+  axios.get("/api/auth/logout").then((res) => {
     persistor.purge();
     navigate("/home");
   });
